a83fd8a51bb690ae6ab77734abcfa53c3420477c
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-03-01 12:35:00 +0100

    nuove lenti

---
+++ runDavide.sh
@@ -0,0 +1,20 @@
+python3 PSFConvolution_Trilinear_IDW.py tree double-gauss-1930 4.0 4.0
+python3 PSFConvolution_Trilinear_IDW.py field double-gauss-1930 4.0 4.0
+python3 PSFConvolution_Trilinear_IDW.py tree double-gauss-1930 5.6 5.0
+python3 PSFConvolution_Trilinear_IDW.py tree double-gauss-1930 8.0 5.0
+python3 PSFConvolution_Trilinear_IDW.py field double-gauss-1930 5.6 5.0
+python3 PSFConvolution_Trilinear_IDW.py field double-gauss-1930 8.0 5.0
+
+python3 PSFConvolution_Trilinear_IDW.py field kreitzer-tele 2.8 10.0
+python3 PSFConvolution_Trilinear_IDW.py field kreitzer-tele 4.0 10.0
+python3 PSFConvolution_Trilinear_IDW.py field kreitzer-tele 8.0 10.0
+python3 PSFConvolution_Trilinear_IDW.py tree kreitzer-tele 2.8 10.0
+python3 PSFConvolution_Trilinear_IDW.py tree kreitzer-tele 4.0 10.0
+python3 PSFConvolution_Trilinear_IDW.py tree kreitzer-tele 8.0 10.0
+
+python3 PSFConvolution_Trilinear_IDW.py field fisheye-ii 4.0 0.5
+python3 PSFConvolution_Trilinear_IDW.py field fisheye-ii 5.6 0.5
+python3 PSFConvolution_Trilinear_IDW.py field fisheye-ii 8.0 0.5
+python3 PSFConvolution_Trilinear_IDW.py tree fisheye-ii 4.0 0.5
+python3 PSFConvolution_Trilinear_IDW.py tree fisheye-ii 5.6 0.5
+python3 PSFConvolution_Trilinear_IDW.py tree fisheye-ii 8.0 0.5
--- runGenerator.sh
+++ runGenerator.sh
@@ -1,21 +1,24 @@
-#python3 kernelGenerator.py double-gauss-1930 21 4.0 2.8
-#python3 kernelGenerator.py double-gauss-1930 21 5.0 2.8
+python3 kernelGenerator.py double-gauss-1930 21 4.0 4.0
+python3 kernelGenerator.py double-gauss-1930 21 5.0 5.6
+python3 kernelGenerator.py double-gauss-1930 21 5.0 8.0
 
-#python3 kernelGenerator.py kreitzer-tele 21 10.0 5.6
+python3 kernelGenerator.py kreitzer-tele 21 10.0 2.8
+python3 kernelGenerator.py kreitzer-tele 21 10.0 4.0
+python3 kernelGenerator.py kreitzer-tele 21 10.0 8.0
 
-#python3 kernelGenerator.py fisheye-ii 21 0.5 2.8
+python3 kernelGenerator.py fisheye-ii 21 0.5 4.0
+python3 kernelGenerator.py fisheye-ii 21 0.5 5.6
+python3 kernelGenerator.py fisheye-ii 21 0.5 8.0
 
-#python3 kernelGenerator.py canon-zoom 21 2.5 2.8
-#python3 kernelGenerator.py canon-zoom 21 3.0 2.8
-#python3 kernelGenerator.py canon-zoom 21 6.4 2.8
-python3 kernelGenerator.py canon-zoom 21 12.0 2.8
+python3 kernelGenerator.py canon-zoom 21 2.5 4.0
+python3 kernelGenerator.py canon-zoom 21 2.5 5.6
+python3 kernelGenerator.py canon-zoom 21 2.5 1.4
 
-#python3 kernelGenerator.py petzval 21 3.0 2.8
-#python3 kernelGenerator.py petzval 21 5.0 2.8
-#python3 kernelGenerator.py petzval 21 6.4 2.8
-python3 kernelGenerator.py petzval 21 12.0 2.8
+python3 kernelGenerator.py petzval 21 5.0 4.0
+python3 kernelGenerator.py petzval 21 5.0 5.6
+python3 kernelGenerator.py petzval 21 5.0 8.0
 
-#python3 kernelGenerator.py brendel-tessar 21 3.0 2.8
-#python3 kernelGenerator.py brendel-tessar 21 5.0 2.8
-#python3 kernelGenerator.py brendel-tessar 21 6.4 2.8
-#python3 kernelGenerator.py brendel-tessar 21 12.0 2.8
+python3 kernelGenerator.py brendel-tessar 21 5.0 4.0
+python3 kernelGenerator.py brendel-tessar 21 5.0 5.6
+python3 kernelGenerator.py brendel-tessar 21 5.0 8.0
+
